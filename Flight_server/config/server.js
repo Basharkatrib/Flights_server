62ce266143aeb5e0a2cebd9af73b31af9f6cd11c
Your Name <you@example.com>
2025-04-17 00:30:25 +0300

    server7 commit

--- Flight_server/config/server.js
+++ Flight_server/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  // url: 'https://flights-server.onrender.com',
+  url: 'https://flights-server.onrender.com',
   proxy: { koa: true },
   app: {
     keys: env.array('APP_KEYS'),
