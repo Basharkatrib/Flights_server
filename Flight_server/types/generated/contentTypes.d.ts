716492fae9e03b260be71e611e645d568588bd21
Your Name <you@example.com>
2025-04-16 14:30:33 +0300

    server6 commit

--- Flight_server/types/generated/contentTypes.d.ts
+++ Flight_server/types/generated/contentTypes.d.ts
@@ -1180,7 +1180,6 @@ export interface PluginUsersPermissionsUser
   };
   options: {
     draftAndPublish: false;
-    timestamps: true;
   };
   attributes: {
     blocked: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
@@ -1194,6 +1193,7 @@ export interface PluginUsersPermissionsUser
       Schema.Attribute.SetMinMaxLength<{
         minLength: 6;
       }>;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
